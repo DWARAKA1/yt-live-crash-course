0fbe6b79452094b43c07c1560aaaaa5d881770d4
sunnysavita10 <engsunny3365@gmail.com>
2026-02-17 21:16:10 +0530

    custom module updated

--- research_and_analyst/exception/custom_exception.py
+++ research_and_analyst/exception/custom_exception.py
@@ -5,6 +5,7 @@ from typing import Optional, cast
 
 class ResearchAnalystException(Exception):
     def __init__(self, error_message, error_details: Optional[object] = None):
+        
         # Normalize message
         if isinstance(error_message, BaseException):
             norm_msg = str(error_message)
